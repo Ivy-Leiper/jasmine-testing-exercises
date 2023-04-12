c82b74b6476d50d6f480f1f14476482789358e5b
leiper-mike <74512712+leiper-mike@users.noreply.github.com>
2023-04-12 16:18:54 -0400

    implentation of calculator & added tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,31 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment({amount: 100000, years: 3, rate: 10})).toBe("3226.72")
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const monthly = calculateMonthlyPayment({amount: 100000, years: 3, rate: 10})
+  expect(typeof monthly).toBe("string");
+  expect(monthly.split(".")[1].length).toBe(2);
 });
 
-/// etc
+it("should throw an error with values", function(){
+  expect(function()
+  {
+    calculateMonthlyPayment({amount: "notanumber", years: 3, rate: 10});
+  }).
+  toThrowError(Error,"Invalid inputs: Principle: notanumber, Rate: 0.008333333333333333, Years: 36")
+  expect(function()
+  {
+    calculateMonthlyPayment({amount: "notanumber", years: "notanumber", rate: "10"});
+  }).
+  toThrowError(Error,"Invalid inputs: Principle: notanumber, Rate: 0.008333333333333333, Years: NaN")
+
+  expect(function()
+  {
+    calculateMonthlyPayment({amount: undefined, years: undefined, rate: undefined});
+  })
+  .toThrowError(Error,"Invalid inputs: Principle: undefined, Rate: NaN, Years: NaN")
+});
+
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,39 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const amountEl = document.getElementById("loan-amount");
+  const yearsEl = document.getElementById("loan-years");
+  const rateEl = document.getElementById("loan-rate");
+  amountEl.value = 100000;
+  yearsEl.value = 3;
+  rateEl.value = 10;
+  update()
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  const monthly = calculateMonthlyPayment(values);
+  updateMonthly(monthly);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const principle = values.amount;
+  const rate = values.rate / 1200;
+  const years = values.years * 12;
+  
+  if(typeof principle !== "number" || typeof rate !== "number" || typeof years != "number")
+    throw new Error(`Invalid inputs: Principle: ${principle}, Rate: ${rate}, Years: ${years}`)
+  return ((principle * rate)/(1-(1+rate) ** (-years))).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyEl = document.getElementById("monthly-payment");
+  monthlyEl.innerText = monthly;
 }
